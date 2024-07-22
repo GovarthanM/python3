2951e2be89b6168b6211d630cc40dda5013532f9
GOVARTHAN M <139614860+GovarthanM@users.noreply.github.com>
2024-07-22 18:59:48 +0530

    Update Set .difference() Operation.py

---
+++ Set .difference() Operation.py
@@ -0,0 +1,15 @@
+n = int(input())
+s1 = set(map(int, input().split()))
+b = int(input())
+s2 = set(map(int, input().split()))
+
+print(len(s1.difference(s2)))
+
+
+#input: 5
+#       1 2 3 4 5
+#       3
+#       5 6 7
+
+
+#output: 4
